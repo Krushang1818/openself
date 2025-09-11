46b9a477b2dd926f8150ef16ba8da4fb51223d04
Aniket-508 <pawaraniket508@gmail.com.com>
2025-09-11 23:33:36 +0530

    chore: polished landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="max-w-4xl flex-1 mx-auto px-4 sm:px-6 lg:px-8 pt-20 flex flex-col md:flex-row pb-16">
+    <main className="flex-1 view-container max-w-[1200px] px-4 pt-20 flex flex-col md:flex-row pb-16">
       {/* Left side - Call to action */}
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start">
         <div className="max-w-md text-center md:text-left">
@@ -15,7 +15,7 @@ export default function Home() {
             100% free & open source
           </Badge>
 
-          <h1 className="text-[32px] font-bold mb-4 flex items-center justify-center md:justify-start gap-4 flex-wrap">
+          <h1 className="text-[32px] font-bold mb-4 flex items-center justify-center md:justify-start gap-2 whitespace-nowrap">
             Cursor for Resumes
             <Image
               src="/highlight-pointer.png"
@@ -27,13 +27,15 @@ export default function Home() {
           </h1>
 
           <p className="text-base text-neutral-600 mb-[30px] text-center md:text-left">
-            Turn your resume/LinkedIn
-            <br /> into a professional website.
+            Create polished resumes using natural language effortlessly.
           </p>
 
           <div className="relative flex flex-col items-center w-full md:w-fit">
-            <Link href={ROUTES.IMPORT}>
-              <Button className="relative group px-6 py-3 h-auto text-base">
+            <Button
+              className="relative group px-6 py-3 h-auto text-base"
+              asChild
+            >
+              <Link href={ROUTES.IMPORT}>
                 <div className="h-[120px] w-10 bg-gradient-to-r from-white/10 via-white/50 to-white/10 absolute blur-sm -rotate-45 -left-16 group-hover:left-[150%] duration-500 delay-200" />
                 <Image
                   src="/sparkle.png"
@@ -42,10 +44,10 @@ export default function Home() {
                   height={20}
                   className="mr-2"
                 />
-                <span className="relative">Upload Resume</span>
+                <span className="relative">Create Resume</span>
                 <BorderBeam colorFrom="#5d5d5d" colorTo="#ffffff" />
-              </Button>
-            </Link>
+              </Link>
+            </Button>
 
             <p className="text-sm text-neutral-500 mt-4 text-center">
               No signup required!
@@ -55,15 +57,15 @@ export default function Home() {
       </div>
 
       {/* Right side - Preview */}
-      <div className="w-full md:w-1/2 flex justify-center items-center flex-1 relative max-h-[700px] min-w-[50%] lg:min-w-[500px]">
-        <div className="absolute inset-0 -bottom-4 rounded-3xl bg-black/5 blur-xl h-full"></div>
+      <div className="w-full md:w-1/2 flex justify-center items-center flex-1 relative">
+        <div className="absolute inset-0 rounded-3xl bg-black/3 blur-xl h-full"></div>
         <Image
           src="/cv-home.png"
           alt="CV Website Preview"
           width={0}
           height={0}
           sizes="100vw"
-          className="h-auto w-full"
+          className="h-full w-auto border relative object-contain rounded-2xl"
         />
       </div>
     </main>
